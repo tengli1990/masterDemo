adf891ad6ff65b8ce47a12f58a732f392ed651ec
liteng <576152693@qq.com>
2017-12-15 16:49:20 +0800

    add simpleVue project

---
+++ simpleVue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div name="App">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				msg:"HELLO WORLD"
+			}
+		}
+	}
+</script>
+<style>
+	
+	
+</style>
